0903fdf7b93c8b833d314d9e7c225dd03bf04ee6
TheCoderArif <mdarifulislam18539@gmail.com>
2026-07-15 15:22:45 +0600

    adding dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const port = process.env.PORT || 5000;
+const port = config.port;
 
 async function main() {
     try {
+        await prisma.$connect();
+        console.log("Database connected successfully!");
         app.listen(port , () => {
             console.log(`Server is running on port: ${port}`);
         })
     } catch (error : any) {
         console.error("Error:", error);
+        await prisma.$disconnect();
         process.exit(1);
     }
 };
