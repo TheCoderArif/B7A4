d38917fcb12c3af706a0eaf1a476fa6646b3a0d2
TheCoderArif <mdarifulislam18539@gmail.com>
2026-07-16 15:25:13 +0600

    adding prisma multiple schema

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,5 +7,6 @@ dotenv.config({
 
 export default {
     port : process.env.PORT,
-    database_url : process.env.DATABASE_URL
+    database_url : process.env.DATABASE_URL,
+    app_url : process.env.APP_URL
 }
